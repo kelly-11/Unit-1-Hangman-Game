534d461fd70f6dec5faca9e1947c96ff27f2d68a
kelly-11 <kellyguzman2010@gmail.com>
2023-03-25 18:25:55 -0700

    Updated my css/html & started my JS

---
+++ script.js
@@ -0,0 +1,39 @@
+//Variables for Categories, letters, guesses, screen
+
+let selections = ['Movies', 'T.V Shows', 'Actors']
+let keys = []
+let guesses = []
+let screen = []
+
+function init() {
+    screen = 
+    guesses = [null, null, null, null, null, null, null]
+    keys = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
